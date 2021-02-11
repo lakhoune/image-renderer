88b1ab72131ed9eb7486ca44233f239546d490a3
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-02-11 10:34:31 +0100

    added format checking and cleaning fucntions

--- README.md
+++ README.md
@@ -14,3 +14,24 @@ Make a post request to the root url. The body should contain at least the follow
 * ``data``: This attribute should contain the data which should be visualized as a ``json array`` (or string of json array). Each ``json`` object represents a data point of the chart. The first property of the object designates the label of that data point and should therefore be of type ``string``
 * ``chartType``: This property should contain the type of Google chart that should be displayed (Pay attention to case sensitivity). If omitted, the chart will be rendered as a `Piechart` 
 * `options`: Holds optional drawing instructions. For more on this see the  [official Google Charts documentation](https://developers.google.com/chart/interactive/docs/customizing_charts)
+* `clean`: If false than the data will not be cleaned before visualization. Cleaning removes all entries which are not of the maximum entry size. This ensures that all data entries have the same size which is necessary to be displayed as a chart. 
+>If ``clean`` is omitted, the data will be cleaned by default.
+
+Here's a sample request
+```
+# What a sample request might look like. If you are using vscode, you can use REST client to send this request
+POST  http://localhost:3000/ HTTP/1.1
+content-type: application/json
+
+{
+ "data": [{"stars":"3.7500"},{"author":"adam","stars":"5.0000"},{"author":"lakhoune","stars":"3.5000"},{"author":"ben","stars":"4.3333"},{"author":"aaron","stars":"1.0000"}],
+ "chartType":"BarChart",
+ "options":{
+     "title":"Average stars per user",
+     "hAxis":{
+         "title":"Average stars"
+     }
+ },
+ "clean":true 
+}
+```
--- index.js
+++ index.js
@@ -4,52 +4,52 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const { renderGoogleChart } = require('google-charts-node/lib/render');
 app.use(bodyParser.json());
+const defaultChartType = 'PieChart';
 
 app.get('/', async (req, res) => {
-  // Render the chart to image
+  // Renders an example chart
   const image = await GoogleChartsNode.render(drawChart);
   res.header('Content-Type', 'image/png');
   res.send(image);
 });
 
-app.post('/', async (req, res) => {
-  console.log(req.body);
-
-  let data = req.body.data;
-  if (typeof data == 'string') {
-    data = JSON.parse(data);
-  }
-  // console.log(data);
-  const drawchartString = formatChartString(data, req.body.chartType, req.body.options);
-  // console.log(drawchartString);
-  await GoogleChartsNode.render(drawchartString)
-    .then((image) => {
-      // console.log(image.buffer);
-      // res.header('Content-Type', 'image/png');
-      // res.send(image);
-      res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': image.length,
-      });
-      res.end(image);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).send(err);
-    });
-});
+app.post('/', renderCustomChart);
 
 app.post('/customQuery', async (req, res) => {
   console.log(req.body);
   if (req.body.customQuery) {
-    let data = req.body.customQuery;
-    if (typeof data == 'string') {
-      data = JSON.parse(data);
+    req.body.data = req.body.customQuery;
+    renderCustomChart(req, res);
+  } else {
+    console.log('wrong type');
+    res.status(400).send('This type of query is not currently supported. ');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+async function renderCustomChart(req, res) {
+  console.log(req.body);
+
+  let data = req.body.data; //data as json array or jsonString
+  if (typeof data == 'string') {
+    data = JSON.parse(data);
+  }
+  try {
+    data = data.map((element) => [...Object.values(element)]); //json array might contain entries as object. This make sure that they are an array
+    if (req.body.clean == false) {
+    } else {
+      //Recommended. This makes sure that the data can actually be visualized
+      data = cleanData(data);
     }
-    // console.log(data);
+    checkData(data);
+
     const drawchartString = formatChartString(data, req.body.chartType, req.body.options);
-    // console.log(drawchartString);
+
     await GoogleChartsNode.render(drawchartString)
       .then((image) => {
         // console.log(image.buffer);
@@ -65,15 +65,11 @@ app.post('/customQuery', async (req, res) => {
         console.error(err);
         res.status(400).send(err);
       });
-  } else {
-    console.log('wrong type');
-    res.status(400).send('This type of query is not currently supported. ');
+  } catch (error) {
+    console.error(error);
+    res.status(422).send(error.message);
   }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+}
 
 function drawChart() {
   let options = {
@@ -94,10 +90,15 @@ function drawChart() {
   let chart = new google.visualization.ColumnChart(container);
   chart.draw(data, options);
 }
-
-function formatChartString(query, chartType, options) {
+/**
+ * Format the chartString for google-charts-node.
+ * @param {*} data
+ * @param {*} chartType
+ * @param {*} options
+ */
+function formatChartString(data, chartType, options) {
   if (!chartType) {
-    chartType = 'PieChart';
+    chartType = defaultChartType;
   }
   if (options) {
     options = JSON.stringify(options);
@@ -106,24 +107,24 @@ function formatChartString(query, chartType, options) {
   }
   let s1 = '';
 
-  for (const key of Object.keys(query[0])) {
-    //Sometimes the values inside the query are represented as a string even though they might be a number in reality
-    if (isActuallyANumber(query[0][key])) {
+  for (const key of Object.keys(data[0])) {
+    //Sometimes the values inside the data are represented as a string even though we want to display it as a number
+    if (isActuallyANumber(data[0][key])) {
       s1 += `data.addColumn('number', '${key}');`;
     } else {
-      s1 += `data.addColumn('${typeof query[0][key]}', '${key}');`;
+      s1 += `data.addColumn('${typeof data[0][key]}', '${key}');`;
     }
   }
 
   let s2 = `data.addRows([`;
-  for (const object of query) {
-    //Sometimes the values inside the query are represented as a string even though they might be a number in reality
+  for (const object of data) {
+    //Sometimes the values inside the data are represented as a string even though they might be a number in reality
     let arr = Object.values(object).map((val) => (isActuallyANumber(val) ? Number(val) : val));
     s2 += `${JSON.stringify(arr)},`;
   }
   s2 += ']);';
 
-  let result = `
+  return `
   var data = new google.visualization.DataTable();
   ${s1}
   ${s2}
@@ -132,11 +133,43 @@ function formatChartString(query, chartType, options) {
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.${chartType}(document.getElementById('chart_div'));
   chart.draw(data, options);`;
-
-  //   console.log(result);
-  return result;
 }
-
+/**
+ * checks if n is actually a number
+ * @param {number} n
+ */
 function isActuallyANumber(n) {
   return typeof n == 'number' || !isNaN(Number(n));
+}
+/**
+ * checks if data can be visualized with  Google Charts
+ * @param {any[][]} data an array of arrays
+ */
+function checkData(data) {
+  if (data.length < 1) {
+    throw new Error('no data provided');
+  }
+
+  let dataEntrySize = data[0].length; //size which all entries need to be
+  for (const arrayEntry of data) {
+    if (arrayEntry.length != dataEntrySize) {
+      throw new Error('Data entries have different size');
+    }
+  }
+}
+/**
+ * removes all array entries smaller than the largest array. In this way each entry will have the same length. This is necessary for Google Charts
+ * @param {any[][]} data an array of arrays
+ */
+function cleanData(data) {
+  data = data.map((element) => [...Object.values(element)]);
+  let maxEntrySize = getMaxEntrySize(data);
+  return data.filter((entry) => entry.length == maxEntrySize);
+}
+/**
+ * Get the maximum length of the entries
+ * @param {any[][]} data an array of arrays
+ */
+function getMaxEntrySize(data) {
+  return data.reduce((maxSize, curr) => (curr.length > maxSize ? curr.length : maxSize), 0);
 }
